8f9bcb6a55d44d32f7fbdf7fed4b9702e80e315d
João M. Mrqs <joao.marcelo.mm@gmail.com>
2024-02-07 23:24:12 -0300

    Fixed form logic and added new composable for form posts
    Changed form logic to be compatible with server side and delegated post logic to new composable dedicated to handle and post form data.

--- src/components/Contact/form/ContactForm.jsx
+++ src/components/Contact/form/ContactForm.jsx
@@ -1,57 +1,26 @@
-import { serverUrl } from "../../../url";
-export function ContactForm() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = JSON.stringify(() => Object.fromEntries(formData.entries()));
+import { postFormData } from '../../../composables/DataRequests.js';
 
-    fetch(`${serverUrl}/api/contact/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: data,
-    }).then((res) => console.log(res.json()));
-  };
-  return (
-    <form
-      className="contact-form"
-      action={`${serverUrl}/api/contact/`}
-      method="post"
-      onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        name="name"
-        id="name"
-        placeholder="Your Name"
-        required
-      />
-      <div className="inline-input">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email Address"
-          required
-        />
-        <input type="tel" name="phone" id="phone" placeholder="Phone Number" />
-      </div>
-      <input
-        type="text"
-        name="subject"
-        id="subject"
-        placeholder="Subject"
-        required
-      />
-      <textarea
-        name="message"
-        id="message"
-        rows="6"
-        placeholder="Your Message"
-        required
-      ></textarea>
-      <button type="submit">Send</button>
-    </form>
-  );
+export function ContactForm() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        postFormData(e);
+    };
+    return (
+        <form className="contact-form" method="post" onSubmit={handleSubmit}>
+            <input type="text" name="name" id="name" placeholder="Your Name" required />
+            <div className="inline-input">
+                <input type="email" name="email" id="email" placeholder="Email Address" required />
+                <input type="tel" name="telephone" id="telephone" placeholder="Phone Number" />
+            </div>
+            <input type="text" name="subject" id="subject" placeholder="Subject" required />
+            <textarea
+                name="message"
+                id="message"
+                rows="6"
+                placeholder="Your Message"
+                required
+            ></textarea>
+            <button type="submit">Send</button>
+        </form>
+    );
 }
--- src/composables/DataRequests.js
+++ src/composables/DataRequests.js
@@ -1,4 +1,4 @@
-import { serverUrl } from "../url.js";
+import { serverUrl } from '../url.js';
 
 /**
  * Fetches data from the specified path and updates the state with the retrieved data.
@@ -8,7 +8,22 @@ import { serverUrl } from "../url.js";
  * @return {Promise<void>} A Promise that resolves when the state is updated
  */
 export async function fetchIntoState(path, setState) {
-  const response = await fetch(`${serverUrl}${path}`);
-  const data = await response.json();
-  setState(data);
+    const response = await fetch(`${serverUrl}${path}`);
+    const data = await response.json();
+    setState(data);
+}
+
+export async function postFormData(e) {
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    const res = await fetch(`${serverUrl}/api/contact/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    const resMessage = await res.json();
+    console.log(resMessage);
 }
